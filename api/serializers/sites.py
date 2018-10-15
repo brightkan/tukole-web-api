a59f48f89bbc8f952adedc10a11e5b9d674eca85
Remo Samuel Paul <remosam42@gmail.com>
2018-10-16 01:27:28 +0300

    serializer clean up

--- api/serializers/sites.py
+++ api/serializers/sites.py
@@ -1,13 +1,10 @@
-
-
-	
 from rest_framework import serializers
+
 from api.models.sites import Sites
 
-##//////////////////
 
 class SitesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Sites
-        fields = ('id', 'site_name', 'location','clientId', 'ackStatus', 'workStatus', 'archivedStatus','created_at', 'updated_at')	
-    
+        fields = ('id', 'site_name', 'location', 'clientId', 'ackStatus', 'workStatus', 'archivedStatus')
+        read_only_fields = ('created', 'updated')
