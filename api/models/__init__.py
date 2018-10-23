29a1a7f44e20b5abf1bea7b342e301557684da5f
Remo Samuel Paul <remosam42@gmail.com>
2018-10-23 17:16:15 +0300

    added workspace to __init__ file

--- api/models/__init__.py
+++ api/models/__init__.py
@@ -3,11 +3,13 @@ from api.models.machinery import Machinery
 from api.models.materials import Material
 from api.models.sites import Sites
 from api.models.users import User
+from api.models.workspaces import Workspace
 
 __all__ = [
     'User',
     'Fleet',
     'Machinery',
     'Sites',
-    'Material'
+    'Material',
+    'Workspace'
 ]
--- api/serializers/sites.py
+++ api/serializers/sites.py
@@ -6,5 +6,7 @@ from api.models.sites import Sites
 class SitesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Sites
-        fields = ('id', 'site_name', 'location_lat', 'location_long', 'start_date', 'expected_end_date', 'archivedStatus', 'clientId', 'ackStatus', 'current_stage', 'archivedStatus' , 'workspace_id', 'created_at', 'updated_at')
-       # read_only_fields = ('created', 'updated')
+        fields = (
+        'id', 'site_name', 'location_lat', 'location_long', 'start_date', 'expected_end_date', 'archivedStatus',
+        'clientId', 'ackStatus', 'current_stage', 'archivedStatus', 'workspace_id', 'created_at', 'updated_at')
+    # read_only_fields = ('created', 'updated')
