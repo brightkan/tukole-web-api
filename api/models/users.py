3e64fae9fce01ef28fb9def6b541bd0486cd22c1
Remo Samuel Paul <remosam42@gmail.com>
2018-10-24 12:25:02 +0300

    added user creation with workspace

--- api/models/users.py
+++ api/models/users.py
@@ -9,7 +9,7 @@ from api.models import Workspace
 
 
 class MyUserManager(BaseUserManager):
-    def _create_user(self, email, password, **extra_fields):
+    def create_user(self, email, password, **extra_fields):
         if not email:
             raise ValueError('The Email must be set')
         email = self.normalize_email(email)
@@ -27,7 +27,7 @@ class MyUserManager(BaseUserManager):
             raise ValueError('Superuser must have is_staff=True.')
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
-        return self._create_user(email, password, **extra_fields)
+        return self.create_user(email, password, **extra_fields)
 
 
 class AbstractEmailUser(AbstractBaseUser, PermissionsMixin):
@@ -75,8 +75,15 @@ class User(AbstractEmailUser, TimeStampedModel):
     last_name = models.CharField(max_length=255, null=True, blank=True)
     contract_type = models.CharField(max_length=150, choices=contract_type_choices)
     phone_number = models.CharField(max_length=150)
-    workspace = models.ForeignKey(to=Workspace, related_name="user_workspace", null=True, blank=True,
-                                  on_delete=models.SET_NULL)
 
     def __str__(self):
         return "%s %s %s" % (self.id, self.first_name, self.last_name)
+
+
+class UserWorkSpace(TimeStampedModel):
+    user = models.ForeignKey(to=User, related_name="user_workspace_user", on_delete=models.CASCADE, db_index=True)
+    workspace = models.ForeignKey(to=Workspace, related_name="user_workspace_workspace", on_delete=models.CASCADE,
+                                  db_index=True)
+
+    class Meta:
+        unique_together = ('user', 'workspace',)
--- api/serializers/users.py
+++ api/serializers/users.py
@@ -1,16 +1,34 @@
+from copy import deepcopy
+
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 from api.models import User
+from api.models.users import UserWorkSpace
 
 
 class UserSerializer(ModelSerializer):
+    workspace = serializers.CharField(required=True, write_only=True)
+    password = serializers.CharField(required=True, write_only=True)
+    user_workspace = serializers.SerializerMethodField(read_only=True)
+
+    def get_user_workspace(self, obj):
+        workspace = UserWorkSpace.objects.filter(user=obj).values_list('workspace', flat=True)
+        return workspace[0]
 
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
+        all_data = deepcopy(validated_data)
+        print(all_data)
+        workspace = all_data.pop('workspace')
+        print(workspace)
+        user = User.objects.create_user(**all_data)
+        user_workspace, created = UserWorkSpace.objects.get_or_create(
+            workspace_id=workspace,
+            user=user
+        )
         return user
 
     class Meta:
         model = User
-        fields = ('id', 'first_name', 'last_name', 'email',
-                  'type', 'contract_type', 'phone_number')
-        write_only_fields = ('password',)
+        fields = ('id', 'first_name', 'last_name', 'email', 'type', 'contract_type',
+                  'phone_number', 'workspace', 'password', 'user_workspace')
