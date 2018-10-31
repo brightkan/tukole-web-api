4d8b81fd8f7d83c5c404c1efecbdf40768124645
Noah Francis <noahwalugembe@gmail.com>
2018-10-31 10:10:29 +0300

    Added filtering on siteboqs api

--- api/views/siteboqs.py
+++ api/views/siteboqs.py
@@ -1,4 +1,4 @@
-				
+from django_filters.rest_framework import DjangoFilterBackend				
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
@@ -10,6 +10,8 @@ class SiteboqViewSet(ModelViewSet):
     permission_classes = (IsAuthenticated,)
     serializer_class = SiteboqSerializer
     queryset = Siteboq.objects.all()
+    filter_backends = (DjangoFilterBackend,)
+    filter_fields = ('site','material','user')
 
 
     
