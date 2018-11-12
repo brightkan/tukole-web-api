fa03b09002ce337a26dbcd33c5a96d4a321fce39
Noah Francis <noahwalugembe@gmail.com>
2018-11-12 11:43:52 +0300

    Added Siterole notification

--- api/models/siteroles.py
+++ api/models/siteroles.py
@@ -12,3 +12,13 @@ class Siterole(TimeStampedModel):
     site  = models.ForeignKey(Site, on_delete=models.CASCADE, null=True, blank=True,related_name="siteroles_site")
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, related_name="siteroles_user")
     userrole = models.ForeignKey(UserRoles, on_delete=models.CASCADE, null=True, blank=True,related_name="siterole_userrole")                            							  
+    
+    def save(self, *args, **kwargs):
+        site = self.site
+        user = self.user
+        userrole = self.userrole
+        
+        note='You have bean added to site'+site+'as'+userrole
+        print(note)        
+        p = Notification(user=user,notification=note)
+        p.save()
