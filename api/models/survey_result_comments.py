95e0deb6e4459ae2dce07705b62153b26edaf18b
Noah Francis <noahwalugembe@gmail.com>
2018-11-12 11:51:41 +0300

    Added SurveyResultComment notification

--- api/models/survey_result_comments.py
+++ api/models/survey_result_comments.py
@@ -3,9 +3,18 @@ from django.db import models
 from model_utils.models import TimeStampedModel
 
 from api.models.survey_results import SurveyResult
+from api.models import User
 
 
 class SurveyResultComment(TimeStampedModel):
     survey_result= models.ForeignKey(SurveyResult, on_delete=models.CASCADE, null=True, blank=True, related_name="SurveyResult_survey_result")
     comment= models.CharField(max_length=255)
     readStatus=models.BooleanField(default=False)
+    surveyor= models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, related_name="surveyresultscoments_surveyor")
+        
+    def save(self, *args, **kwargs):
+            surveyor = self.surveyor
+            note='There is a new comment on the uploaded survey resutls'
+            print("There is a new comment on the uploaded survey resutls")        
+            p = Notification(user=surveyor,notification=note)
+            p.save()    
