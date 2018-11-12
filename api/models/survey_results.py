6c00c1823d2afff74c51407b002611950361d02a
Noah Francis <noahwalugembe@gmail.com>
2018-11-12 11:46:32 +0300

    Added SurveyResult notification

--- api/models/survey_results.py
+++ api/models/survey_results.py
@@ -15,3 +15,25 @@ class SurveyResult(TimeStampedModel):
     acceptStatus=models.BooleanField(default=False)
 
 
+    def save(self, *args, **kwargs):
+        surveyor = self.surveyor
+        acceptStatus = self.acceptStatus
+        note1='Pending Review'
+        note2='survey_results_accepted'
+        note3='Survey results were rejected'
+    
+        if acceptStatus == None:
+            print ("Pending Review")
+            p = Notification(user=surveyor,notification=note1)
+            p.save()            
+    
+    
+        elif result == True: # shouldn't do this
+            print ("survey_results_accepted")
+            p = Notification(user=surveyor,notification=note2)
+            p.save()            
+    
+        else:
+            print ("Survey results were rejected" ) 
+            p = Notification(user=surveyor,notification=note3)
+            p.save()    
