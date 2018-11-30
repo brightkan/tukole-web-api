6dc9105453e55d308d24c7184e528ac3288e138f
Remo Samuel Paul <remosam42@gmail.com>
2018-11-30 14:14:06 +0300

    added running out of material

--- api/models/materials.py
+++ api/models/materials.py
@@ -11,3 +11,4 @@ class Material(TimeStampedModel):
                                   related_name="material_workspace")
     measurement = models.CharField(max_length=150, null=True)
     unit_cost = models.CharField(max_length=50, null=True)
+    running_out = models.BooleanField(default=False)
