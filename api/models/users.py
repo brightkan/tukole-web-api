0a5047ab391d42f51edd35d985248204c8cc87f8
Remo Samuel Paul <remosam42@gmail.com>
2019-01-03 13:17:40 +0300

    added superuser creation

--- api/models/users.py
+++ api/models/users.py
@@ -9,16 +9,16 @@ from api.models import Workspace
 
 
 class MyUserManager(BaseUserManager):
-    def create_user(self, email, password, **extra_fields):
-        if not email:
-            raise ValueError('The Email must be set')
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+    def create_user(self, id, password, **extra_fields):
+        if not id:
+            raise ValueError('The Id must be set')
+        # email = self.normalize_email(email)
+        user = self.model(id=id, **extra_fields)
         user.set_password(password)
         user.save()
         return user
 
-    def create_superuser(self, email, password, **extra_fields):
+    def create_superuser(self, id, password, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('is_active', True)
@@ -27,7 +27,7 @@ class MyUserManager(BaseUserManager):
             raise ValueError('Superuser must have is_staff=True.')
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
-        return self.create_user(email, password, **extra_fields)
+        return self.create_user(id, password, **extra_fields)
 
 
 class AbstractEmailUser(AbstractBaseUser, PermissionsMixin):
