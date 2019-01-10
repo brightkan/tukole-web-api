4e0a84f0d5543b1560daef9ef23d47ab3adcb38b
Remo Samuel Paul <remosam42@gmail.com>
2019-01-10 15:40:56 +0300

    added status to fleet

--- api/models/fleets.py
+++ api/models/fleets.py
@@ -8,12 +8,16 @@ from api.models.fleet_types import FleetType
 # Create your models here.
 
 class Fleet(TimeStampedModel):
+    status_choices = (
+        ('available', 'available'),
+        ('broken_down', 'broken_down'),
+        ('assigned', 'assigned')
+    )
     name = models.CharField(max_length=50, null=True)
     vehicle_type = models.ForeignKey(to=FleetType, on_delete=models.CASCADE, null=True, blank=True,
-                                  related_name="fleet_vehicle_type")
+                                     related_name="fleet_vehicle_type")
     uuid = models.CharField(max_length=50, null=True)
     humanUuid = models.CharField(max_length=50, null=True)
-    status = models.CharField(max_length=50, null=True)
+    status = models.CharField(max_length=150, null=True, choices=status_choices)
     workspace = models.ForeignKey(to=Workspace, on_delete=models.CASCADE, null=True, blank=True,
                                   related_name="fleet_workspace")
-
