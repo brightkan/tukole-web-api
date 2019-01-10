c105a40c0499b086036845496ec6eb5474f5e40a
Remo Samuel Paul <remosam42@gmail.com>
2019-01-10 15:23:35 +0300

    added history to fleet

--- api/serializers/sitefleets.py
+++ api/serializers/sitefleets.py
@@ -18,3 +18,18 @@ class UserSitefleetSerializer(serializers.ModelSerializer):
 class UserSiteFleetRequestSerializer(serializers.Serializer):
     user = serializers.CharField()
     site = serializers.CharField()
+
+
+class UserSiteFleetHistorySerializer(serializers.ModelSerializer):
+    user_full_name = serializers.SerializerMethodField(read_only=True)
+    fleet = serializers.SerializerMethodField(read_only=True)
+
+    def get_user_full_name(self, obj):
+        return "%s %s" % (obj.user.first_name, obj.user.last_name)
+
+    def get_fleet(self, obj):
+        return obj.site_fleet.fleet.name
+
+    class Meta:
+        model = UserSiteFleet
+        fields = ('id', 'user', 'user_full_name', 'site_fleet', 'fleet', 'created')
--- api/views/fleets.py
+++ api/views/fleets.py
@@ -1,17 +1,31 @@
 # Create your views here.
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK
 
+from api.models import UserSiteFleet
 from api.models.fleets import Fleet
 from api.serializers.fleets import FleetSerializer
+from api.serializers.sitefleets import UserSiteFleetHistorySerializer
 
-
-# Create your views here.
 
 class FleetViewset(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticated,)
     queryset = Fleet.objects.all()
     serializer_class = FleetSerializer
     filter_backends = (DjangoFilterBackend,)
-    filter_fields = ('workspace','vehicle_type')
+    filter_fields = ('workspace', 'vehicle_type')
+
+    @action(methods=['get'], detail=True, url_path='history', url_name="history")
+    def fleet_history(self, request, pk):
+        assignments = UserSiteFleet.objects.filter(site_fleet__fleet__id=pk)
+
+        if assignments:
+            print(assignments)
+            data = UserSiteFleetHistorySerializer(assignments, many=True).data
+        else:
+            data = {"error": "User is not attached to any site"}
+        return Response(data=data, status=HTTP_200_OK)
