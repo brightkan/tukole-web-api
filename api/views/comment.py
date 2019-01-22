30ff41c8e0ba9c4d47715098e82be3d86f406abf
Remo Samuel Paul <remosam42@gmail.com>
2019-01-22 15:11:34 +0300

    added notification for comments

--- api/views/comment.py
+++ api/views/comment.py
@@ -3,8 +3,10 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
-from api.models import Comment
+from api.models import Comment, User
 # Create your views here.
+from api.models.notifications import Notification
+from api.models.siteroles import Siterole
 from api.serializers.comment import CommentSerializer
 
 
@@ -14,3 +16,15 @@ class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     filter_backends = (DjangoFilterBackend,)
     filter_fields = ('site', 'user')
+
+    def perform_create(self, serializer):
+        super().perform_create(serializer)
+        affected_teams = serializer['affected_teams']
+        site = serializer['site']
+        description = serializer['description']
+        affected_teams = affected_teams.split(',')
+        for team in affected_teams:
+            user_ids = Siterole.objects.filter(site_id=site, role=team).values_list('user_id', flat=True)
+            users = User.objects.filter(id__in=user_ids)
+            for user in users:
+                Notification.objects.create(description=description, user=user)
