bd3da83d0f7793b998488b9fa92cdd8e700fe187
Remo Samuel Paul <remosam42@gmail.com>
2019-02-07 15:49:48 +0300

    added fleet humanuuid and name to fleet fuel

--- api/serializers/fuel.py
+++ api/serializers/fuel.py
@@ -10,6 +10,15 @@ class FuelSerializer(serializers.ModelSerializer):
 
 
 class FleetFuelRequestSerializer(serializers.ModelSerializer):
+    fleet_name = serializers.SerializerMethodField()
+    fleet_humanuuid = serializers.SerializerMethodField()
+
+    def get_fleet_name(self, obj):
+        return obj.fleet.name
+
+    def get_fleet_humanuuid(self, obj):
+        return obj.fleet.humanUuid
+
     class Meta:
         model = FleetFuelRequest
         fields = ('id', 'fleet', 'user', 'requested_fuel_in_litres', 'received_fuel_in_litres', 'total_fuel_amount',
