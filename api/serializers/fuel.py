c94bee6342a349f3a49f98eca17a9f2e20ac117c
Remo Samuel Paul <remosam42@gmail.com>
2019-02-07 16:29:18 +0300

    fixing fleet fuel serializer

--- api/serializers/fuel.py
+++ api/serializers/fuel.py
@@ -22,7 +22,8 @@ class FleetFuelRequestSerializer(serializers.ModelSerializer):
     class Meta:
         model = FleetFuelRequest
         fields = ('id', 'fleet', 'user', 'requested_fuel_in_litres', 'received_fuel_in_litres', 'total_fuel_amount',
-                  'mileage_at_fuelling_time', 'status', 'refuel_reject_reason', 'created')
+                  'mileage_at_fuelling_time', 'status', 'refuel_reject_reason', 'created', 'fleet_name',
+                  'fleet_humanuuid')
 
 
 class FuelReceiptSerializer(serializers.ModelSerializer):
