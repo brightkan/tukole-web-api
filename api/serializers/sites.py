bfca5db50502349d7359c8895712b026b98b5af2
Remo Samuel Paul <remosam42@gmail.com>
2019-03-31 10:52:33 +0300

    fixed user site api serializer

--- api/serializers/sites.py
+++ api/serializers/sites.py
@@ -39,7 +39,13 @@ class SiteUserRoleSerializer(serializers.ModelSerializer):
             'id', 'site_name', 'site_deleted', 'site_accessible', 'site_surveyed', 'location_lat',
             'location_long', 'start_date', 'survey_date', 'expected_end_date', 'clientId',
             'current_stage', 'archivedStatus', 'workspace', 'surveyor', 'ackStatus', 'ack_user',
-            'ack_date', 'survay_time', 'created', 'modified', 'user_role', 'can_client_view_survey_reports'
+            'ack_date', 'survay_time', 'created', 'modified', 'can_client_view_survey_reports', 'site_contact_person',
+            'site_contact_phone_number', 'site_location', 'site_accepted', 'company', 'site_connected',
+            'site_connection_date', 'site_connection_request_acknowledged', 'site_ready_for_connection',
+            'number_of_site_fleet', 'number_of_members_on_site', 'site_image', 'site_completed',
+            'isp_works_complete', 'osp_works_complete', 'ofc_works_complete', 'site_powering_complete',
+            'original_trenching_distance', 'current_trenching_distance', 'site_drawing', 'site_address',
+            'site_usd_rate', 'user_role'
         )
 
 
