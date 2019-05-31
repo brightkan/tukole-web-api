1d1c61e8017aa8cafae2feacd728bfe54dd1e8e8
Remo Samuel Paul <remosam42@gmail.com>
2019-05-31 15:00:59 +0300

    added object_id to repair ticket

--- api/serializers/repairs.py
+++ api/serializers/repairs.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 
-from api.models import RepairHistory
+from api.models import RepairHistory, Fleet, Machinery
 from api.models.repairs import RepairTicket
+from api.models.tools import Tool
 
 
 class RepairHistorySerializer(serializers.ModelSerializer):
@@ -11,9 +12,31 @@ class RepairHistorySerializer(serializers.ModelSerializer):
 
 
 class RepairTicketSerializer(serializers.ModelSerializer):
+
+    def get_object_id(self):
+        object_type = self.type
+        human_uuid = self.humanUuid
+        if object_type == 'fleet':
+            fleet = Fleet.objects.filter(humanUuid=human_uuid).first()
+            if fleet:
+                return fleet.id
+            return ""
+
+        elif object_type == 'machine':
+            machine = Machinery.objects.filter(humanUuid=human_uuid).first()
+            if machine:
+                return machine.id
+            return ""
+        elif object_type == 'tool':
+            tool = Tool.objects.filter(humanUuid=human_uuid)
+            if tool:
+                return tool.id
+            return ""
+
     class Meta:
         model = RepairTicket
         fields = (
+            'object_id'
             'reported_by',
             'time_reported',
             'time_acknowledged',
