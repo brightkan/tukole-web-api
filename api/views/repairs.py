bd739965b9c0a6a1889612cad54ff96c3644bbe5
Abdu Ssekalala <assekalala@gmail.com>
2019-09-03 14:19:18 +0300

    fixing fleet repair ticket creation

--- api/views/repairs.py
+++ api/views/repairs.py
@@ -1,8 +1,9 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-from api.models import RepairHistory
+from api.models import RepairHistory, Fleet
 from api.models.repairs import RepairTicket
 from api.serializers.repairs import RepairHistorySerializer, RepairTicketSerializer
 
@@ -22,3 +23,14 @@ class RepairTicketViewSet(viewsets.ModelViewSet):
     filter_backends = (DjangoFilterBackend,)
     filter_fields = ('reported_by', 'acknowledged_by', 'repairs_verified', 'repairs_started', 'requisition_required',
                      'assessment_verified', 'perform_fix', 'repairs_complete')
+
+    def create(self, request, *args, **kwargs):
+        human_uuid = request.data['humanUuid']
+        if Fleet.objects.filter(humanUuid__icontains=human_uuid).exists():
+            return super().create(request, *args, **kwargs)
+        else:
+            response = {
+                "status": False,
+                "message": "No fleet found with that human uuid"
+            }
+            return Response(response, status=status.HTTP_201_CREATED)
