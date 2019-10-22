9d878296495e6ba116563d2507ccb8b4d8c150fa
Remo Samuel Paul <remosam42@gmail.com>
2019-10-22 16:39:03 +0300

    fixing multiple user login

--- api/views/login.py
+++ api/views/login.py
@@ -27,33 +27,42 @@ class MyTokenObtainPairSerializer(serializers.Serializer):
         password_ = attrs['password']
         data = {}
         authed_user = None
+        workspace = Workspace.objects.filter(name__icontains=workspace_).first()
 
-        user_ = User.objects.filter(email=email_).first()
-        if user_:
-            authed_user = authenticate(**{'id': user_.id, 'password': password_})
-            print("##################################################")
-            print(authed_user)
+        user_ = User.objects.filter(email=email_).exists()
+        user_instances = User.objects.filter(email=email_)
+        user_ids = User.objects.filter(email=email_).values_list('id', flat=True)
+        user_ids = list(user_ids)
 
-        if authed_user is None:
+        authed_user_ids = []
+        for user_id in user_ids:
+            authed_user = authenticate(**{'id': user_id, 'password': password_})
+            if authed_user:
+                authed_user_ids.append(user_id)
+
+        if not authed_user_ids:
             raise serializers.ValidationError('No active account found with the given credentials')
 
-        else:
-            refresh = self.get_token(authed_user)
-            workspace = Workspace.objects.filter(name__icontains=workspace_).first()
-            print(workspace)
-            part_of_workspace = False
-            if workspace:
-                if UserWorkSpace.objects.filter(workspace=workspace, user=authed_user).first():
-                    part_of_workspace = True
-                    data['refresh'] = text_type(refresh)
-                    data['access'] = text_type(refresh.access_token)
-                    data['user_id'] = authed_user.id
-                    data['user_role'] = authed_user.role
-                    data['user_type'] = authed_user.type
-                    data['workspace'] = workspace.id
-                    data['company'] = authed_user.company.id if authed_user.company else ""
-            data['part_of_workspace'] = part_of_workspace
+        if not workspace:
+            raise serializers.ValidationError('No workspace with that name found in the system')
 
+        user_workspace = UserWorkSpace.objects.filter(workspace=workspace, user_id__in=authed_user_ids)
+
+        if not user_workspace:
+            raise serializers.ValidationError('User is not part of that workspace')
+
+        else:
+            authed_user = authenticate(**{'id': user_workspace[0].user.id, 'password': password_})
+            refresh = self.get_token(authed_user)
+            part_of_workspace = True
+            data['refresh'] = text_type(refresh)
+            data['access'] = text_type(refresh.access_token)
+            data['user_id'] = authed_user.id
+            data['user_role'] = authed_user.role
+            data['user_type'] = authed_user.type
+            data['workspace'] = workspace.id
+            data['company'] = authed_user.company.id if authed_user.company else ""
+            data['part_of_workspace'] = part_of_workspace
             return data
 
 
